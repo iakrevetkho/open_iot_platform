6110c3e3b9a0dbd3f23000b4f0eaa6fdb3b764db
iakrevetkho <40871973+iakrevetkho@users.noreply.github.com>
2018-07-06 22:40:41 +0300

    Update setup.sh
    Add Node-Red installation

--- setup.sh
+++ setup.sh
@@ -2,15 +2,21 @@
 
 
 echo "Install packages"
-sudo apt-get update
-sudo apt-get install make erlang rebar build-essential
+apt-get update
+apt-get install make erlang rebar build-essential mongodb nodejs npm
 
+echo "Install Node-Red"
+npm install -g --unsafe-perm node-red
 
-echo "Get source"
+echo "Config Node-Red to autostart on boot"
+cp nodered.service /lib/systemd/system/nodered.service
+systemctl enable nodered.service
+
+echo "Get source EMQ"
 file="emq-relx"
 if [ -f "$file" ]
 then
-	echo "$file found."
-else
-	echo "$file not found."
+	echo "$file found. Remove old."
+	rm -r $file
 fi
+git clone https://github.com/emqtt/emq-relx.git 2.3.10
