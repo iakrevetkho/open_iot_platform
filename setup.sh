4e15791488b89e124b159b9811abacc3abf5de63
iakrevetkho <40871973+iakrevetkho@users.noreply.github.com>
2018-07-06 23:00:14 +0300

    Update setup.sh
    Add spacers in echo

--- setup.sh
+++ setup.sh
@@ -2,17 +2,24 @@
 
 emq_version="2.3.10"
 
+echo "****************************************************************************************************************"
 echo "Install packages"
 apt-get update
 apt-get install make erlang rebar build-essential mongodb nodejs npm
 
+echo "****************************************************************************************************************"
 echo "Install Node-Red"
 npm install -g --unsafe-perm node-red
 
+echo "****************************************************************************************************************"
 echo "Config Node-Red to autostart on boot"
 cp nodered.service /lib/systemd/system/nodered.service
 systemctl enable nodered.service
+echo "****************************************************************************************************************"
+echo "Start Node-Red service"
+sudo service nodered start
 
+echo "****************************************************************************************************************"
 echo "Get source EMQ"
 if [ -f "$file" ]
 then
