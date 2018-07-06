b296303c557cf0889d1a3a7e5d90b429c94f15c5
iakrevetkho <40871973+iakrevetkho@users.noreply.github.com>
2018-07-06 21:11:42 +0300

    Update setup.sh
    Add installation of dep packages

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "Start deploying"
+echo "Install packages"
+sudo apt-get update
+sudo apt-get install make erlang rebar build-essential
+
+
+echo "Get source"
+file="emq-relx"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+fi
